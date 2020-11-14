2b1ef4a95e3e0d6f6d6a667f11772c52712a12cd
Tharuka <tharukavishwajiths@gmail.com>
2020-11-14 10:47:16 +0100

    added structure to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', redirectTo: '/login', pathMatch: 'full'
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // preload all modules; optionally we could
+    // implement a custom preloading strategy for just some
+    // of the modules (PRs welcome 😉)
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
